9e2e5bf49ed5cb882d320efc6f547315b11ce084
Jing <jingkcheng@hotmail.com>
2019-06-27 23:37:48 -0400

    Updates to Spently section

--- client/src/components/Spently.js
+++ client/src/components/Spently.js
@@ -4,9 +4,12 @@ export default class Spently extends React.Component{
     render(){
         return(
             <div className="spently-background">
+                <div className="divider">
+                    <h2>Enough about me, now let's talk about Spently!</h2>
+                </div>
                 <div className="spently-container">
                     <div>
-                        <img src={require('../imgs/spently.png')} alt="Spently"/>
+                        <img src={require('../imgs/spently.png')} className="spently-img" alt="Spently"/>
                         <h2>Supercharged Store E-mails App</h2>
                     </div>
                 </div>
