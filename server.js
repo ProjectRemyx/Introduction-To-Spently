35b5451367359edca8fc2fc2723f1739478e639e
Jing <jingkcheng@hotmail.com>
2019-06-30 11:48:02 -0400

    Push to heroku

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+//Require dependencies
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path'); 
 
 const pricing = require('./routes/api/pricing');
 
@@ -10,12 +12,27 @@ app.use(bodyParser.json());
 
 const db = require('./config/keys').mongoURI;
 
+//Connect to db
 mongoose
     .connect(db, { useNewUrlParser:true })
     .then(() => console.log('Connected to database'))
     .catch(err => console.log(err));
 
+//Use routes
 app.use('/api/pricing', pricing);
+
+/* Built React client on server */
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production')
+{
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    //If anything that's not /api/pricing build our React app
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
